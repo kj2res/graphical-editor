5f86600f9d6e77d7e73e29ba4a88f4717180eda8
King James Torres <tcorp.kingjames@gmail.com>
2015-03-31 13:04:58 +0800

    Preparing unit testing

---
+++ tests/GraphicalEditorTest.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use GraphicalEditor\GraphicalEditorService;
+
+/**
+ * Editor Commands Test
+ * @author King James Torres <tcopr.kingjames@gmail.com>
+ */
+class GraphicalEditorTest extends PHPUnit_Framework_TestCase {
+    
+}
