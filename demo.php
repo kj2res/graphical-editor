a0c04dbb8b146d8a81a880b329a21e3a3d56ba02
King James Torres <tcorp.kingjames@gmail.com>
2015-04-01 19:43:24 +0800

    Added Validations

--- demo.php
+++ demo.php
@@ -9,10 +9,34 @@ require_once 'vendor/autoload.php';
 
 use GraphicalEditor\GraphicalEditorService;
 
+/**
+ * [$exit Flag for Exiting the program]
+ * @var integer
+ */
 $exit = 0;
-
-function getArgs( $input ) {
+	
+/**
+ * [getArgs Get the Inputs]
+ * @param  [type] $input [description]
+ * @return [type]        [description]
+ */
+function getArgs( $input ) 
+{
 	return explode( ' ', $input );
+}
+
+/**
+ * [isValidParams checking parameters]
+ * @param  [type]  $args   [description]
+ * @param  [type]  $length [description]
+ * @return boolean         [description]
+ */
+function isValidParams( $args, $length ) 
+{
+	if( count( $args ) !== $length ) {
+		return false;
+	}
+	return true;
 }
 
 $editor = new GraphicalEditorService();
@@ -40,11 +64,26 @@ while( !$exit )
 
 	if( $command === 'I' ) 
 	{	
-		$editor->create( $args[1], $args[2] );
+		if( isValidParams( $args, 3 ) ) 
+		{
+			$editor->create( $args[1], $args[2] );
+		}
+		else 
+		{
+			print "Invalid Command. It should be 'I M N' \n";
+		}
 	}
 	else if( $command === 'L' ) 
 	{
-		$editor->setColor( $args[1], $args[2], $args[3] );
+		if( isValidParams( $args, 4 ) ) 
+		{
+			$editor->setColor( $args[1], $args[2], $args[3] );
+		}
+		else
+		{
+			print "Invalid Command. It should be 'L X Y C' \n";
+		}
+		
 	}
 	else if( $command === 'S' ) 
 	{
@@ -59,6 +98,9 @@ while( !$exit )
 				print "\n";
 			}
 		}
+		else {
+			print 'No image created' . "\n";
+		}
 	}
 	else if( $command === 'C' ) 
 	{
@@ -66,15 +108,36 @@ while( !$exit )
 	}
 	else if( $command === 'F' ) 
 	{
-		$editor->fillRegion( $args[1], $args[2], $args[3] );
+		if( isValidParams( $args, 4 ) )
+		{
+			$editor->fillRegion( $args[1], $args[2], $args[3] );
+		}
+		else
+		{
+			print "Invalid Command. It should be 'F X Y C' \n";
+		}
 	}
 	else if( $command === 'V' )
 	{
-		$editor->setVerticalSegment( $args[1], $args[2], $args[3], $args[4] );
+		if( isValidParams( $args, 5 ) )
+		{
+			$editor->setVerticalSegment( $args[1], $args[2], $args[3], $args[4] );
+		}
+		else
+		{
+			print "Invalid Command. It should be 'V X Y1 Y2 C' \n";
+		}
 	}
 	else if( $command === 'H' )
 	{
-		$editor->setHorizontalSegment( $args[1], $args[2], $args[3], $args[4] );
+		if( isValidParams( $args, 5 ) )
+		{
+			$editor->setHorizontalSegment( $args[1], $args[2], $args[3], $args[4] );
+		}
+		else 
+		{
+			print "Invalid Command. It should be 'H X1 X2 Y C' \n";
+		}
 	}
 }
 
